2190210060d1d031075bcf182cbdffd7f797c917
Tina Wang <tina@tina.cafe>
2021-06-15 11:14:07 +0200

    ✏️ Change default project title & description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'ISHITANI FURNITURE',
+    title: 'Tina Nuxt/Tailwind Template',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'ISHITANI FURNITURE' }
+      { hid: 'description', name: 'description', content: 'Tina Nuxt/Tailwind Template' }
     ],
     htmlAttrs: {
       lang: 'en'
