6bdf500670d12f3b07314612fc5ef5c2726ee49e
Tina Wang <tina@tina.cafe>
2021-12-04 15:11:53 +0100

    🏗️ Rolling back to nuxt2

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,7 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/http'
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
