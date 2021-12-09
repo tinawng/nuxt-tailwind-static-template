8b9b933949558f4a97b71da74e20b0408a57cc24
Tina Wang <fabzidentiste@gmail.com>
2021-12-09 22:02:49 +0100

    🔧 Facilitate mobile UI dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,8 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   ssr: false,
-
+  server: { host: "0.0.0.0" },
+  
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Tina Nuxt/Tailwind Template',
