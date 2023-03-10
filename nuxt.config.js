020bdc6bc2f809afc7f4657ffa448ba958852ab1
Tina Wang <fabzidentiste@gmail.com>
2023-03-10 14:36:20 +0100

    ⚡ Add build compression

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  
+  nitro: { compressPublicAssets: true },
 
   modules: [
     ['@nuxtjs/tailwindcss'],
