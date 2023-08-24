1a10d7807a1b8355a8dc4f7986c87289f619d494
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2023-08-24 15:10:51 +0100

    'feat: added tsconfig to enable nuxt auto imports'

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    ssr: false,
+
+    nitro: { compressPublicAssets: true },
+
+    modules: [
+        "@nuxtjs/tailwindcss",
+        ["@pinia/nuxt", { autoImports: ["defineStore", "acceptHMRUpdate"] }],
+    ],
+
+    imports: {
+        dirs: ["stores"],
+    },
+
+    css: ["@/assets/css/base.postcss"],
+
+    app: {
+        head: {
+            title: "Template",
+            htmlAttrs: { lang: "en" },
+            link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Tina Nuxt/Tailwind Template",
+                },
+            ],
+        },
+    },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
-<script setup>
+<script setup lang="ts">
+const count = ref(1);
+
+const increaseCount = () => {
+    count.value++;
+};
+const decreaseCount = () => {
+    count.value--;
+};
 </script>
 
 <template>
-  <div>👋 Hello there</div>
+    <div>
+        <p>👋 Hello there</p>
+        <div class="mt-5 flex items-center gap-5">
+            <button
+                class="bg-red-600 text-white rounded-md px-3 py-1 disabled:bg-red-300 disabled:cursor-not-allowed"
+                :disabled="count === 0"
+                @click="decreaseCount"
+            >
+                Decrease Count
+            </button>
+            <p>Count - {{ count }}</p>
+            <button
+                class="bg-green-600 text-white rounded-md px-3 py-1"
+                @click="increaseCount"
+            >
+                Increase Count
+            </button>
+        </div>
+    </div>
 </template>
 
-<style lang="postcss" scoped>
-</style>
+<style lang="postcss" scoped></style>
